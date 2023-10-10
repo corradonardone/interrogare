3a3b3a0d630c5a07e2a565ad6ea4aac8a36bf9e8
Corrado <48294536+corradonardone@users.noreply.github.com>
2023-10-10 19:48:12 +0200

    Apportate migliorie significative

--- main.py
+++ main.py
@@ -1,37 +1,44 @@
+import os
 import random
 
-def interrogare_alunni(alunni, quanti):
-    if quanti < 1 or quanti > len(alunni):
-        print("Inserisci un numero compreso tra 1 e", len(alunni))
+def interrogare_alunni(classe, quanti):
+    folder_path = "classi"
+    filename_alunni = os.path.join(folder_path, f"alunni_{classe}.txt")
+    filename_interrogati = os.path.join(folder_path, f"alunni_interrogati_{classe}.txt")
+
+    try:
+        with open(filename_alunni, "r") as file:
+            alunni = file.read().splitlines()
+    except FileNotFoundError:
+        print(f"Il file '{filename_alunni}' non esiste. Assicurati di avere un file con i nomi degli alunni per questa classe.")
         return
 
     try:
-        with open("alunni_interrogati.txt", "r") as file:
+        with open(filename_interrogati, "r") as file:
             alunni_interrogati = file.read().splitlines()
     except FileNotFoundError:
         alunni_interrogati = []
 
+    if quanti < 1 or quanti > len(alunni):
+        print(f"Inserisci un numero compreso tra 1 e {len(alunni)}")
+        return
+
     alunni_disponibili = [alunno for alunno in alunni if alunno not in alunni_interrogati]
 
     if not alunni_disponibili:
-        print("Hai interrogato tutti gli alunni disponibili.")
+        print(f"Hai interrogato tutti gli alunni disponibili nella classe {classe}.")
         return
 
     alunni_selezionati = random.sample(alunni_disponibili, quanti)
 
-    print("Gli alunni interrogati sono:")
+    print(f"Gli alunni interrogati nella classe {classe} sono:")
     for alunno in alunni_selezionati:
         print(alunno)
 
-    with open("alunni_interrogati.txt", "a") as file:
+    with open(filename_interrogati, "a") as file:
         for alunno in alunni_selezionati:
             file.write(alunno + "\n")
 
-alunni = [
-    "corrado",
-    "benny",
-    "erika"
-]
-
-nalunni = int(input("Quanti alunni vuoi interrogare?"))
-interrogare_alunni(alunni, nalunni)
+classe_da_interrogare = input("Inserisci la classe da interrogare: ")
+quantita_da_interrogare = int(input("Quanti alunni vuoi interrogare?"))
+interrogare_alunni(classe_da_interrogare, quantita_da_interrogare)
